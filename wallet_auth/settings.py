a1215828b665bf1093fbe8ca4d2f99af9b21a2c7
Ennyola <medunoyeeni@gmail.com>
2023-03-20 14:41:08 +0100

    added db engine

--- wallet_auth/settings.py
+++ wallet_auth/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "wallet_auth.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("DB_ENGINE"),
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
